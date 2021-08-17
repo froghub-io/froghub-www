e41780d5d24797fe60feaca9e29f3c6b8f379ff9
oldman <597800451@qq.com>
2021-08-17 10:38:32 +0800

    init project

---
+++ component/Tool.js
@@ -0,0 +1,22 @@
+Vue.component('Tool', {
+    data: function () {
+        return {
+        }
+    },
+    template: `
+     <div class="tool-content">
+        <div class="tool-content-block">
+            算法
+        </div>
+        <div class="tool-content-block">
+            调度
+        </div>
+        <div class="tool-content-block">
+            调度
+        </div>
+        <div class="tool-content-block">
+            调度
+        </div>
+    </div>
+    `
+})
