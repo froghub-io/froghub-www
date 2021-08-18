9cb367833e09849993ab432c9b5d229401bbbd59
pig <pig@gmail.com>
2021-08-18 23:43:18 +0800

    官网

--- component/Tool.js
+++ component/Tool.js
@@ -1,21 +1,76 @@
 Vue.component('Tool', {
     data: function () {
         return {
+            operates: [
+                {
+                    key: "algorithm",
+                    icon: "./static/img/navigation/algorithm.png",
+                    label: "算法"
+                },
+                {
+                    key: "dispatch",
+                    icon: "./static/img/navigation/dispatch.png",
+                    label: "调度"
+                },
+                {
+                    key: "prover",
+                    icon: "./static/img/navigation/prover.png",
+                    label: "时空证明"
+                },
+                {
+                    key: "cloudc2",
+                    icon: "./static/img/navigation/cloudc2.png",
+                    label: "Cloud C2"
+                },
+                {
+                    key: "gas",
+                    icon: "./static/img/navigation/gas.png",
+                    label: "GAS"
+                },
+                {
+                    key: "block",
+                    icon: "./static/img/navigation/block.png",
+                    label: "孤块"
+                },
+                {
+                    key: "sector",
+                    icon: "./static/img/navigation/sector.png",
+                    label: "扇区修复"
+                },
+                {
+                    key: "transfer",
+                    icon: "./static/img/navigation/transfer.png",
+                    label: "集群迁移"
+                },
+                {
+                    key: "fpga",
+                    icon: "./static/img/navigation/fpga.png",
+                    label: "FPGA"
+                },
+                {
+                    key: "monitor",
+                    icon: "./static/img/navigation/monitor.png",
+                    label: "全时监控"
+                }
+            ]
+        }
+    },
+    methods: {
+        onAnchor(key) {
+            let top = document.getElementById(key).offsetTop;
+            window.scrollTo({
+                left: 0,
+                top: top,
+                behavior: "smooth"
+            });
         }
     },
     template: `
-     <div class="tool-content">
-        <div class="tool-content-block">
-            算法
-        </div>
-        <div class="tool-content-block">
-            调度
-        </div>
-        <div class="tool-content-block">
-            调度
-        </div>
-        <div class="tool-content-block">
-            调度
+     <div class="tool-content tool-box">
+        <img class="tool-logo" src="./static/img/navigation/flogo.png" @click="onAnchor('app')">
+        <div class="tool-content-block" v-for="item in operates" @click="onAnchor(item.key)">
+            <img class="tool-content-icon" :src="item.icon">
+            {{item.label}}
         </div>
     </div>
     `
